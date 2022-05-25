8bfaa97f8410e9ecb07b8b88e484ac368f10367b
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-05-25 14:42:14 -0600

    added edit and delete routes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //DEPENDENCIES
 const express = require('express');
+const req = require('express/lib/request');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Product = require("./models/product.js");
-
+const methodOverride = require("method-override");
 
 //DATABASE CONNECTON
 mongoose.connect(process.env.DATABASE_URL, {
@@ -21,6 +22,7 @@ db.on("disconnected", () => console.log("mongo disconnected"));
 
 //MIDDLEWARE
 app.use(express.urlencoded({extend: true}));
+app.use(methodOverride('_method'));
 
 //SEED
 // const productSeed = require('./models/productSeed.js');
@@ -46,17 +48,40 @@ app.get("/store/new", (req, res) => {
     res.render("new.ejs");
 })
 //DESTROY.DELETE
+app.delete("/store/:id", (req, res) => {
+    Product.findByIdAndDelete(req.params.id, (err, data) => {
+        res.redirect("/store")
+    });
+});
 
 //UPDATE.PUT
+app.put("/store/:id", (req, res) => {
+    Product.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true,
+        },
+  (error, updateProduct) => {
+      res.redirect(`/store/${req.params.id}`)
+  });
+});
 
 //CREATE.POST
 app.post("/store", (req, res) => {
-    Product.create(req.body, (erroe, createProduct) => {
+    Product.create(req.body, (error, createdProduct) => {
         res.redirect('/store');
     });
 });
 
 //EDIT.GET
+app.get("/store/:id/edit", (req, res) => {
+    Product.findById(req.params.id, (error, foundProduct) => {
+        res.render('edit.ejs', {
+            product: foundProduct,
+        });
+    });
+}) ;
 
 //SHOW.GET
 app.get('/store/:id', (req, res) => {
